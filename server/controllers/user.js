d54e5cde0af978a1dc591c0f8e165f35961ec01c
Santosh Kumar <atharw010@gmail.com>
2024-08-23 18:04:12 +0530

    User register, login and profile routes and controller completed

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,8 @@
+import { compare } from "bcrypt";
 import { User } from "../models/user.js";
+import { sendToken } from "../utils/features.js";
+import { TryCatch } from "../middlewares/error.js";
+import { ErrorHandler } from "../utils/utility.js";
 
 //Create new User:-
 const newUser = async (req, res) => {
@@ -9,7 +13,7 @@ const newUser = async (req, res) => {
     url: "ksjnfkdfwfsvkjfd",
   };
 
-  await User.create({
+  const user = await User.create({
     name,
     username,
     password,
@@ -17,13 +21,29 @@ const newUser = async (req, res) => {
     avatar,
   });
 
-  res.status(201).json({
-    message: "User Created successfully",
+  sendToken(res, user, 201, "User created Successfully");
+};
+
+const login = TryCatch(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username }).select("+password");
+
+  if (!user) return next(new ErrorHandler("Invalid password OR Username", 404));
+
+  const isMatch = await compare(password, user.password);
+
+  if (!isMatch)
+    return next(new ErrorHandler("Invalid Password OR Username", 404));
+
+  sendToken(res, user, 200, `Welcome back, ${user.name}`);
+});
+
+const getMyProfile = async (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: "req.user",
   });
 };
 
-const login = (req, res) => {
-  res.send("Hye from server");
-};
-
-export { login, newUser };
+export { login, newUser, getMyProfile };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { login, newUser } from "../controllers/user.js";
+import { getMyProfile, login, newUser } from "../controllers/user.js";
 import { singalAvatar } from "../middlewares/multer.js";
 
 const app = express.Router();
 
 app.post("/new", singalAvatar, newUser);
-
 app.post("/login", login);
+
+// User must be logged in to access the routes
+
+app.get("/profile", getMyProfile);
 
 export default app;
